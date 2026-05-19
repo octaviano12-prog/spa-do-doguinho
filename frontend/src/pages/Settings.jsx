af2225db98d693bd53ae6e8e309d5824f82394e5
octaviano12-prog <octaviano12@gmail.com>
2026-05-19 18:07:43 -0300

    Update Settings.jsx

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import CrudPage from "../components/CrudPage";
 export default function Settings() {
   return <CrudPage title="Configurações" subtitle="Configurações visuais e textos do site" endpoint="settings" searchFields={["setting_key","setting_value"]} defaults={{setting_key:"",setting_value:""}}
