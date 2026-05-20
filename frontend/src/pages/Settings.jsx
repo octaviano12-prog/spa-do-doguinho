878f9d6412626a3f6c27936a698f4e8544c3bd1f
octaviano12-prog <octaviano12@gmail.com>
2026-05-19 21:46:40 -0300

    Delete frontend directory

--- frontend/src/pages/Settings.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import CrudPage from "../components/CrudPage";
-export default function Settings() {
-  return <CrudPage title="Configurações" subtitle="Configurações visuais e textos do site" endpoint="settings" searchFields={["setting_key","setting_value"]} defaults={{setting_key:"",setting_value:""}}
-    columns={[{key:"setting_key",label:"Chave"},{key:"setting_value",label:"Valor"}]}
-    fields={[{name:"setting_key",label:"Chave"},{name:"setting_value",label:"Valor",type:"textarea",full:true}]} />;
-}
