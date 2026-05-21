7b3fd3a2487954097b7531df91f4d8896458a05b
octaviano12-prog <octaviano12@gmail.com>
2026-05-21 20:32:47 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const db = require("./config/db");
 
 const app = express();
@@ -7,8 +9,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+/* =========================
+   API HEALTH
+========================= */
+
 app.get("/health", (req, res) => {
-  res.json({ ok: true, app: "SPA do Doguinho API funcionando" });
+  res.json({
+    ok: true,
+    app: "SPA do Doguinho API funcionando"
+  });
 });
 
 app.get("/api/health", async (req, res) => {
@@ -26,6 +35,23 @@ app.get("/api/health", async (req, res) => {
       error: error.message
     });
   }
+});
+
+/* =========================
+   FRONTEND REACT
+========================= */
+
+const frontendPath = path.join(
+  process.cwd(),
+  "../public_html"
+);
+
+app.use(express.static(frontendPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(frontendPath, "index.html")
+  );
 });
 
 module.exports = app;
