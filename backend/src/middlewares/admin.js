c04e5ff458c76f238feae58849920b42b3237eb0
octaviano12-prog <octaviano12@gmail.com>
2026-05-21 17:36:01 -0300

    Delete backend directory

--- backend/src/middlewares/admin.js
+++
@@ -1,6 +0,0 @@
-module.exports = function adminOnly(req, res, next) {
-  if (req.user?.role !== "admin") {
-    return res.status(403).json({ message: "Acesso permitido apenas para administrador" });
-  }
-  return next();
-};
--- backend/src/routes/availabilityRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const c = require("../controllers/availabilityController");
-
-// Público: usado pelo site para mostrar horários disponíveis
-router.get("/slots", c.slots);
-
-module.exports = router;
