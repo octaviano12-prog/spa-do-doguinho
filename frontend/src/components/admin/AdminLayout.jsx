3f5df66bc5a863cced0a1e1166ed1122a8efac17
octaviano12-prog <octaviano12@gmail.com>
2026-05-21 19:41:46 -0300

    Update AdminLayout.jsx

--- frontend/src/components/admin/AdminLayout.jsx
+++ frontend/src/components/admin/AdminLayout.jsx
@@ -1,15 +1,16 @@
 import AdminSidebar from "./AdminSidebar";
 import AdminHeader from "./AdminHeader";
+
 import PageTransition from "../ui/PageTransition";
 
 export default function AdminLayout({
   children
 }) {
   return (
-    <div className="flex bg-gray-100 min-h-screen">
+    <div className="flex min-h-screen">
       <AdminSidebar />
 
-      <div className="flex-1">
+      <div className="flex-1 overflow-hidden">
         <AdminHeader />
 
         <PageTransition>
