b71f87d73a8229a562d20d2278828a94e2adb03d
octaviano12-prog <octaviano12@gmail.com>
2026-05-21 17:03:56 -0300

    Usar API do dominio atual

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,5 +1,31 @@
 import axios from "axios";
 
+const apiBaseUrl =
+  import.meta.env.VITE_API_URL ||
+  `${window.location.origin}/api`;
+
+const client = axios.create({
+  baseURL: apiBaseUrl,
+});
+
+client.interceptors.request.use((config) => {
+  const token = localStorage.getItem("spa_token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+client.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error)
+);
+
+export default client;
+import axios from "axios";
+
 const client = axios.create({
   baseURL: "https://spadodoguinho.com.br/api",
 });
