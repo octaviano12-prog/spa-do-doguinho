049aeb977ab8e65b502b7c116689038984534e71
octaviano12-prog <octaviano12@gmail.com>
2026-05-21 18:32:03 -0300

    Delete backend directory

--- backend/src/routes/cascadeRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const auth = require("../middlewares/auth");
-const cascade = require("../controllers/cascadeController");
-
-router.delete("/pets/:id/cascade", auth, cascade.deletePetCascade);
-router.delete("/customers/:id/cascade", auth, cascade.deleteCustomerCascade);
-
-module.exports = router;
