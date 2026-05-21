4792271d54f00154d07a4f88fb665985370e0c36
octaviano12-prog <octaviano12@gmail.com>
2026-05-21 19:38:03 -0300

    Create EstoquePage.jsx

---
+++ frontend/src/pages/admin/EstoquePage.jsx
@@ -0,0 +1,42 @@
+import CrudPage from "../../components/ui/CrudPage";
+
+export default function EstoquePage() {
+  return (
+    <CrudPage
+      title="Estoque"
+      endpoint="stock"
+      fields={[
+        {
+          key: "name",
+          label: "Produto"
+        },
+
+        {
+          key: "quantity",
+          label: "Quantidade"
+        },
+
+        {
+          key: "sale_price",
+          label: "Preço"
+        }
+      ]}
+      columns={[
+        {
+          key: "name",
+          label: "Produto"
+        },
+
+        {
+          key: "quantity",
+          label: "Quantidade"
+        },
+
+        {
+          key: "sale_price",
+          label: "Preço"
+        }
+      ]}
+    />
+  );
+}
